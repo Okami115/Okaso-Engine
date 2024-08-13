090940fddc404029e69d39ed879791a7423481c7
Dario <otrantodario@gmail.com>
2024-08-12 22:53:32 -0300

    Add Separation Of Base Classes
    Add Base Game Class
    Add Renderer Class
    Add Window Class

---
+++ Okaso/src/BaseGame/BaseGame.cpp
@@ -0,0 +1,41 @@
+﻿#include "baseGame.h"
+
+Okaso_Engine::Okaso_Engine()
+{
+    initGame();
+}
+
+Okaso_Engine::~Okaso_Engine()
+{
+    endGame();
+}
+
+void Okaso_Engine::initGame()
+{
+    /* Initialize the library */
+    if (!glfwInit())
+        return;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = new Window(640, 480, "Okaso_Engine", NULL, NULL);
+
+    if (!window)
+    {
+        glfwTerminate();
+        return;
+    }
+    renderer = new Renderer(window);
+}
+
+void Okaso_Engine::gameLoop()
+{
+    while (!glfwWindowShouldClose(window->getWindow()))
+    {
+        renderer->RenderScreen();
+    }
+}
+
+void Okaso_Engine::endGame()
+{
+    glfwTerminate();
+}
