edd636f0a10ffec1c9e47c1198b7943a37e21efc
Dario <otrantodaro@gmail.com>
2024-09-23 20:13:23 -0300

    Add Shape Summaries

--- Okaso/src/Entity/Entity2D/Shape/Shape.h
+++ Okaso/src/Entity/Entity2D/Shape/Shape.h
@@ -4,9 +4,21 @@
 class EXPORT Shape : public Entity2D
 {
 public:
+	/// <summary>
+	/// Create a new Shape
+	/// </summary>
 	Shape();
+
+	/// <summary>
+	/// Destroy a new Shape
+	/// </summary>
 	~Shape();
+
+	/// <summary>
+	/// Draw a new Shape
+	/// </summary>
 	void Draw() override;
+	
 	unsigned int* VBO;
 	unsigned int* EBO;
 	unsigned int* VAO;
